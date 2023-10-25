df90b93c8dcae60b180de8faf929fc0745ce2eb5
Fiquell <fiquellh@gmail.com>
2023-10-26 01:42:16 +0800

    added bracket lines

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').options} */
 const config = {
+  bracketSameLine: true,
   plugins: ["prettier-plugin-tailwindcss"],
 };
 
