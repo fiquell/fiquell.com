9cc7ded56077431741b3c5861575b0ae923d50fa
Fiquell <fiquellh@gmail.com>
2023-10-28 01:56:38 +0800

    fix typography size

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,11 +5,11 @@ const Header = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <header className="container mt-8 flex justify-between md:mt-5 md:items-center">
+    <header className="container mt-5 flex justify-between md:items-center">
       <Link href="/">
-        <Image src="/logo.png" alt="Logo" width={70} height={70} />
+        <Image src="/logo.png" alt="Logo" width={55} height={55} />
       </Link>
-      <div className="text-xs font-medium text-accent">
+      <div className="text-xs font-medium text-accent md:text-[10px] lg:text-xs">
         <div className="flex flex-col gap-5 md:flex-row md:items-center md:gap-10 lg:gap-16">
           <div className="md:w-48 lg:w-[44rem]">
             <p>FRONTEND DEVELOPER</p>
@@ -21,7 +21,7 @@ const Header = () => {
           </div>
           <Link
             href="/"
-            className="w-fit rounded-full border-2 border-accent px-5 py-2 text-sm text-text">
+            className="w-fit rounded-full border-2 border-accent px-4 py-2 text-sm text-text md:text-xs lg:text-sm">
             SAY HELLO
           </Link>
         </div>
--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -5,7 +5,7 @@ import { developer, end, front } from "~/constants/intro";
 
 const Intro = () => {
   return (
-    <section className="my-20 lg:my-10">
+    <section className="my-10 lg:my-5">
       <div
         className={`text-[11rem] leading-none tracking-tighter text-accent lg:text-[22rem] ${bebasNeue.className}`}>
         <div className="flex flex-col md:flex-row md:items-center md:gap-2 lg:gap-16">
@@ -46,23 +46,23 @@ const Intro = () => {
             ))}
           </div>
         </div>
-        <div className="md:mb-5 md:w-1/4 md:text-[10px] lg:text-lg">
+        <div className="md:w-1/4 md:text-xs lg:mb-10 lg:text-base">
           <div className="mb-20 flex justify-end md:mb-5 lg:mb-20">
-            <p className="w-3/4 leading-normal md:w-full md:leading-tight">
-              <span className="mr-2 text-sm font-medium leading-normal text-accent md:mr-1 md:text-[8px] md:leading-tight lg:mr-2 lg:text-base">
+            <p className="w-3/4 md:w-full">
+              <span className="mr-2 text-sm font-medium text-accent md:text-[10px] md:leading-normal lg:text-sm">
                 ABOUT
               </span>
-              I am a developer based in Lombok, Indonesia, specializing in
-              crafting interactive digital experiences on the web. I collaborate
-              with renowned brands and industry leaders to create engaging
-              online experiences.
+              I am a developer based in Indonesia specializing in crafting
+              interactive digital experiences on the web. I collaborate with
+              renowned brands and industry leaders to create engaging online
+              experiences.
             </p>
           </div>
           <div className="flex items-center justify-between text-accent">
             <p>Scroll down</p>
             <Icon
               icon="majesticons:arrow-down"
-              className="animate-bounce text-2xl md:text-xl lg:text-2xl"
+              className="animate-bounce text-2xl md:text-sm lg:text-xl"
             />
           </div>
         </div>
