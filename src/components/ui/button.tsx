ad88380f847c10018bb97bb1ff8aae731df3db04
Fiquell <fiquellh@gmail.com>
2023-11-05 14:31:37 +0800

    remove children props

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,7 +1,6 @@
 import type { ButtonHTMLAttributes, FC } from "react";
 
 const Button: FC<ButtonHTMLAttributes<HTMLButtonElement>> = ({
-  children,
   className,
   ...props
 }) => {
@@ -10,9 +9,8 @@ const Button: FC<ButtonHTMLAttributes<HTMLButtonElement>> = ({
       {...props}
       className={`rounded-full border-2 border-text px-4 py-2 text-sm font-medium text-text md:text-xs lg:text-sm ${
         className ?? ""
-      }`}>
-      {children}
-    </button>
+      }`}
+    />
   );
 };
 
