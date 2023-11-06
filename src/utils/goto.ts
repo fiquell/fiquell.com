d6ed546a056c30390c40176f5bd1e765b12d7471
Fiquell <fiquellh@gmail.com>
2023-11-06 20:32:45 +0800

    added another parameter method

--- src/utils/goto.ts
+++ src/utils/goto.ts
@@ -1,6 +1,14 @@
 import { gsap } from "gsap";
 
-const goto = (id: string) => {
+const goto = (
+  options:
+    | string
+    | {
+        id: string;
+      },
+) => {
+  const id = typeof options === "string" ? options : options.id;
+
   gsap.to(window, {
     duration: 2,
     scrollTo: {
