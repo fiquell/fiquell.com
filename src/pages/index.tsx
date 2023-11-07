2bc3204e5d68c546ad9786ffacf8c18c86da6e69
Fiquell <fiquellh@gmail.com>
2023-11-07 18:43:16 +0800

    updating html metadata

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,6 +9,24 @@ const Home = () => {
     <>
       <Head>
         <title>Fiquell ✺ Frontend Developer</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="description"
+          content="Crafting captivating websites for exciting online journeys"
+        />
+        <meta
+          name="keywords"
+          content="frontend developer, web development, digital experience specialist"
+        />
+        <meta name="author" content="Fiquell" />
+        <meta property="og:title" content="Fiquell ✺ Frontend Developer" />
+        <meta
+          property="og:description"
+          content="Crafting captivating websites for exciting online journeys"
+        />
+        <meta property="og:image" content="https://fiquell.com/logo.png" />
+        <meta property="og:url" content="https://fiquell.com" />
+        <meta property="og:type" content="website" />
       </Head>
       <Header />
       <main>
