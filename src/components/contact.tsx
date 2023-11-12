504946614f8012c8a62dbcb0ee5378353d5bed20
Fiquell <fiquellh@gmail.com>
2023-11-13 01:25:39 +0800

    make the top margin larger

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -35,7 +35,7 @@ const Contact = () => {
     <section
       ref={sectionRef}
       id="contact"
-      className="mt-10 space-y-10 lg:mt-20 lg:space-y-20">
+      className="mt-20 space-y-10 lg:space-y-20">
       <div className="container">
         <p className="md:mx-auto md:w-1/2 md:text-center md:text-xs lg:text-base">
           If you have any questions, interesting proposals or projects, or if
