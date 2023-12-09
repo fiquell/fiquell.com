6003d58607907db8ca591b92f10f2d16208d593e
Fiquell <fiquellh@gmail.com>
2023-12-09 17:08:32 +0800

    added open graph data

--- src/constants/metadata.ts
+++ src/constants/metadata.ts
@@ -13,13 +13,17 @@ const keywords = [
 ];
 
 export const metadata = [
-  { name: "author", content: "Fiquell" },
   { name: "description", content: description[0] },
+  { name: "author", content: "Fiquell" },
   { name: "keywords", content: keywords.join(", ") },
   { name: "viewport", content: "width=device-width, initial-scale=1.0" },
-  { property: "og:description", content: description[0] },
-  { property: "og:image", content: "https://fiquell.com/logo.png" },
   { property: "og:title", content: title },
+  { property: "og:description", content: description[0] },
   { property: "og:type", content: "website" },
-  { property: "og:url", content: "https://fiquell.com" },
+  { property: "og:url", content: "https://fiquell.com/" },
+  { property: "og:image", content: "https://fiquell.com/logo.png" },
+  { property: "og:image:alt", content: "Logo" },
+  { property: "og:image:type", content: "image/png" },
+  { property: "og:image:width", content: "512" },
+  { property: "og:image:height", content: "512" },
 ];
