977e64c9e96e56b467be3d8a9444da3b7ae15f24
Fiquell <fiquellh@gmail.com>
2024-03-08 21:48:22 +0800

    add homepage content

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,23 @@
 <script lang="ts">
+  import { onMount } from 'svelte';
+  import { expoOut } from 'svelte/easing';
+  import { blur } from 'svelte/transition';
   import '../app.pcss';
+
+  let isPageLoaded = false;
+
+  onMount(() => {
+    isPageLoaded = true;
+  });
 </script>
 
-<slot />
+<svelte:head>
+  <title>Fiquell ✺ Web Developer and Designer</title>
+  <meta name="description" content="Crafting captivating websites for exciting online journeys" />
+</svelte:head>
+
+{#if isPageLoaded}
+  <main transition:blur={{ delay: 500, duration: 2000, easing: expoOut }} class="container">
+    <slot />
+  </main>
+{/if}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<section class="my-28">
+  <p>Web Developer and Designer</p>
+  <p>Lombok, Indonesia</p>
+  <div class="mt-16 max-w-prose text-lg md:text-xl lg:text-2xl">
+    <p>
+      Thanks for dropping by! I'm currently in the midst of revamping my website, so the old one is
+      taking a breather for now. But fear not, the new and improved version is on its way, poised to
+      dazzle and delight! Stay tuned for some seriously exciting updates.
+    </p>
+    <div class="mt-8">
+      <p>Warm regards,</p>
+      <p>Fiquell</p>
+    </div>
+  </div>
+</section>
