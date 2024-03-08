42b553dbd3dbee225680431d44bc169c4b42deb2
Fiquell <fiquellh@gmail.com>
2024-03-08 19:48:06 +0800

    updated base configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,28 @@
 import type { Config } from 'tailwindcss';
+import { fontFamily } from 'tailwindcss/defaultTheme';
 
 const config = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {}
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '2rem',
+        sm: '4rem',
+        md: '6rem',
+        lg: '8rem',
+        xl: '10rem'
+      }
+    },
+    extend: {
+      colors: {
+        background: 'hsl(var(--background) / <alpha-value>)',
+        foreground: 'hsl(var(--foreground) / <alpha-value>)'
+      },
+      fontFamily: {
+        sans: ['"Overpass Variable"', ...fontFamily.sans]
+      }
+    }
   },
   plugins: []
 } satisfies Config;
