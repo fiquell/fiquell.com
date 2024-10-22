aeb9adb75f44ed59c5163bdbd2e153495eb5fee0
Fiquell <fiquellh@gmail.com>
2024-10-22 19:20:38 +0800

    add font and utility modules

---
+++ src/lib/fonts.ts
@@ -0,0 +1,12 @@
+import { Space_Mono, Spline_Sans } from "next/font/google";
+
+export const spline_sans = Spline_Sans({
+  variable: "--font-sans",
+  subsets: ["latin"],
+});
+
+export const space_mono = Space_Mono({
+  weight: "400",
+  variable: "--font-mono",
+  subsets: ["latin"],
+});
