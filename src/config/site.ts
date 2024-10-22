f9fe82ef8f2aa9265710d68af8648a3ddd43e31e
Fiquell <fiquellh@gmail.com>
2024-10-22 19:19:39 +0800

    add site configuration

---
+++ src/config/site.ts
@@ -0,0 +1,10 @@
+export const siteConfig = {
+  name: "Fiquell",
+  tagline: "Indonesian Creative",
+  description:
+    "A multi-faceted Indonesian Creative with a passion for merging aesthetic principles with practical solutions.",
+  url: "https://fiquell.com",
+  color: "#fafafa",
+};
+
+export type SiteConfig = typeof siteConfig;
