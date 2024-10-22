f62387f81d0253983956fd5581c28de5f9d4793b
Fiquell <fiquellh@gmail.com>
2024-10-22 22:49:52 +0800

    refactor indicator component layout and styles

--- src/components/indicator.tsx
+++ src/components/indicator.tsx
@@ -4,15 +4,13 @@ const Indicator = () => {
   }
 
   return (
-    <div className="fixed bottom-2 left-2 z-50">
-      <div className="flex size-8 items-center justify-center rounded-full bg-neutral font-mono text-xs text-neutral-content">
-        <p className="block sm:hidden">xs</p>
-        <p className="hidden sm:max-md:block">sm</p>
-        <p className="hidden md:max-lg:block">md</p>
-        <p className="hidden lg:max-xl:block">lg</p>
-        <p className="hidden xl:max-2xl:block">xl</p>
-        <p className="hidden 2xl:block">2xl</p>
-      </div>
+    <div className="fixed bottom-2 left-2 z-50 font-mono text-xs">
+      <p className="block sm:hidden">xs</p>
+      <p className="hidden sm:max-md:block">sm</p>
+      <p className="hidden md:max-lg:block">md</p>
+      <p className="hidden lg:max-xl:block">lg</p>
+      <p className="hidden xl:max-2xl:block">xl</p>
+      <p className="hidden 2xl:block">2xl</p>
     </div>
   );
 };
