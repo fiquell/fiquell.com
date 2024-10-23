e6a485515a64ed3bcc85fd3b3cd2232a7cfd3e17
Fiquell <fiquellh@gmail.com>
2024-10-23 14:01:09 +0800

    create typography classes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ const RootPage = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center gap-8">
       <div className="max-w-prose text-center">
-        <h1>I am updating a website</h1>
-        <p>
+        <h1 className="id-h1">I am updating a website</h1>
+        <p className="id-paragraph">
           Thanks for dropping by! I am currently refreshing the site I created,
           so the previous version is being re-energized.
         </p>
