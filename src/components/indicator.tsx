8d8bdd4ac71c158811e2a81dd2ff425a8715a18c
Fiquell <fiquellh@gmail.com>
2024-11-04 11:47:21 +0800

    wrap indicator text in a styled container for improved layout

--- src/components/indicator.tsx
+++ src/components/indicator.tsx
@@ -5,12 +5,14 @@ const Indicator = () => {
 
   return (
     <div className="fixed bottom-2 left-2 z-50 font-mono text-xs">
-      <p className="block sm:hidden">xs</p>
-      <p className="hidden sm:max-md:block">sm</p>
-      <p className="hidden md:max-lg:block">md</p>
-      <p className="hidden lg:max-xl:block">lg</p>
-      <p className="hidden xl:max-2xl:block">xl</p>
-      <p className="hidden 2xl:block">2xl</p>
+      <div className="flex size-8 items-center justify-center rounded-full bg-secondary text-secondary-content">
+        <p className="block sm:hidden">xs</p>
+        <p className="hidden sm:max-md:block">sm</p>
+        <p className="hidden md:max-lg:block">md</p>
+        <p className="hidden lg:max-xl:block">lg</p>
+        <p className="hidden xl:max-2xl:block">xl</p>
+        <p className="hidden 2xl:block">2xl</p>
+      </div>
     </div>
   );
 };
