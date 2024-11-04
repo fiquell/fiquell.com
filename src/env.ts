fd2e12d74aaffc1372114b740242e40d3b91d617
Fiquell <fiquellh@gmail.com>
2024-11-04 14:12:04 +0800

    add environment configuration and update dependencies

---
+++ src/env.ts
@@ -0,0 +1,14 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
+
+export const env = createEnv({
+  server: {
+    RESEND_API_KEY: z.string().min(1),
+    RESEND_AUDIENCE_ID: z.string().min(1),
+  },
+  client: {},
+  runtimeEnv: {
+    RESEND_API_KEY: process.env.RESEND_API_KEY,
+    RESEND_AUDIENCE_ID: process.env.RESEND_AUDIENCE_ID,
+  },
+});
