787ffeebd66b1150d94568a3746e105a46323741
Fiquell <fiquellh@gmail.com>
2024-11-04 14:13:56 +0800

    enhance waiting list form with loading state and success message

--- src/components/waiting-list-form.tsx
+++ src/components/waiting-list-form.tsx
@@ -1,13 +1,18 @@
 "use client";
 
 import { zodResolver } from "@hookform/resolvers/zod";
+import * as React from "react";
 import { useForm } from "react-hook-form";
 import { toast } from "sonner";
+import { registerWaitingList } from "~/lib/actions";
 import type { WaitingList } from "~/lib/schemas";
 import { waitingList } from "~/lib/schemas";
 import { cn } from "~/lib/utils";
 
 const WaitingListForm = () => {
+  const [loading, setLoading] = React.useState(false);
+  const [success, setSuccess] = React.useState(false);
+
   const {
     register,
     handleSubmit,
@@ -20,30 +25,54 @@ const WaitingListForm = () => {
     },
   });
 
-  const onSubmit = (values: WaitingList) => {
-    console.log(values);
-    toast.success("You have been added to the waiting list!");
-    reset();
+  const onSubmit = async (values: WaitingList) => {
+    setLoading(true);
+
+    try {
+      const response = await registerWaitingList(values);
+
+      if (response.success) {
+        setSuccess(true);
+        toast.success(response.message);
+        return;
+      }
+
+      reset();
+      toast.error(response.message);
+    } catch (error) {
+      toast.error("Error processing your request.");
+    } finally {
+      setLoading(false);
+    }
   };
+
+  if (success) {
+    return (
+      <p className="id-paragraph">
+        Thank you! You have been added to the waiting list.
+      </p>
+    );
+  }
 
   return (
     <form className="w-full" onSubmit={handleSubmit(onSubmit)}>
       <div className="flex justify-center gap-2">
         <label className="form-control w-full max-w-sm">
           <input
+            {...register("email")}
             type="email"
-            placeholder="Enter you email address"
+            placeholder="Enter your email address"
             className={cn(
               "input input-bordered w-full max-w-sm",
               errors.email && "input-error",
             )}
-            {...register("email")}
           />
           <div className="label">
             <p className="label-text-alt text-error">{errors.email?.message}</p>
           </div>
         </label>
         <button type="submit" className="btn btn-primary">
+          {loading && <span className="loading loading-dots loading-xs" />}
           Notify Me
         </button>
       </div>
--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -1,5 +1,36 @@
-import type { WaitingList } from "~/lib/schemas";
+"use server";
 
-export const registerWaitingList = (values: WaitingList) => {
-  console.log(values);
+import { Resend } from "resend";
+import { env } from "~/env";
+import type { WaitingList } from "~/lib/schemas";
+import { waitingList } from "~/lib/schemas";
+
+const resend = new Resend(env.RESEND_API_KEY);
+
+export const registerWaitingList = async (values: WaitingList) => {
+  try {
+    const { email } = waitingList.parse(values);
+    const { error } = await resend.contacts.create({
+      email: email,
+      unsubscribed: false,
+      audienceId: env.RESEND_AUDIENCE_ID,
+    });
+
+    if (error) {
+      return {
+        success: false,
+        message: "Unable to join the waiting list.",
+      };
+    }
+
+    return {
+      success: true,
+      message: "You've been added to the waiting list.",
+    };
+  } catch (error) {
+    return {
+      success: false,
+      message: "Error processing your request.",
+    };
+  }
 };
